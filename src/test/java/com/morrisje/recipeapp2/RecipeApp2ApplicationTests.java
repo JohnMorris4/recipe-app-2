ecdea02fc7597987505c2db7455327fec02581e1
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-19 03:35:02 -0400

    Setup new MVN project with Spring Initializer

---
+++ src/test/java/com/morrisje/recipeapp2/RecipeApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morrisje.recipeapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
