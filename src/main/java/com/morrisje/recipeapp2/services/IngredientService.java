a03752f8d0ae95ec15a4e3a25f900ff7d2fa0d75
johnmorris4 <johnmorris4@bellsouth.net>
2020-03-30 02:07:29 -0400

    Added Delete action on ingredients

--- src/main/java/com/morrisje/recipeapp2/services/IngredientService.java
+++ src/main/java/com/morrisje/recipeapp2/services/IngredientService.java
@@ -5,4 +5,5 @@ import com.morrisje.recipeapp2.commands.IngredientCommand;
 public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+    void deleteById(Long recipeId, Long idToDelete);
 }
